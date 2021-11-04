6a908f6d293834c1b3b6a544c50578cb66e18e21
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-04 16:57:26 -0400

    added the check database function and added read write functionality to the store

--- public/db.js
+++ public/db.js
@@ -21,3 +21,38 @@ request.onupgradeneeded = function (e) {
 request.onerror = function (e) {
     console.log(`Whoops! ${e.target.errorCode}`);
 };
+
+function checkDatabase() {
+    console.log('check db invoked');
+
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    const store = transaction.objectStore('BudgetStore');
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
+
+                    currentStore.clear();
+                    console.log("Clearing store");
+                }
+            });
+        }
+    };
+}
