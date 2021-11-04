2e0c1000daf7f63ec079b9568e59e91db29c9f15
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-04 16:50:27 -0400

    added the upgrade request for db.js

--- public/db.js
+++ public/db.js
@@ -1,4 +1,19 @@
 let db;
 let budgetVersion;
-
+// new DB request for budget database
 const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+    console.log('Upgrade needed in IndexDB');
+
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+
+    console.log(`DB Updated from version ${oldVersion} to ${ newVersion}`);
+
+    db = e.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
